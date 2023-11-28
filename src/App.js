e69fba06ceb86db9e4790ca1efe52825f716ec6c
xCaWO4x <jchao1@wpi.edu>
2023-11-28 15:00:49 -0500

    Added CreateVenue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import CreateVenue from './boundary/CreateVenue.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Seats4u</h1>
       </header>
+      <main>
+        <CreateVenue />
+      </main>
     </div>
   );
 }
