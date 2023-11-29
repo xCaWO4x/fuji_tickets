8fd46c55aae101541ea3dad1241d48094d6958c0
mattluong <duong.luong2304@gmail.com>
2023-11-28 22:06:01 -0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css'
 import React from 'react'
 import ReactDOM from "react-dom/client"
 // import { BrowserRouter, Routes, Route } from "react-router-dom" 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'; //just added react router dom package - Duong
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Login from './Pages/venueLogin'   //venue login
-import Home from './Pages/Home'
-import Layout from './Pages/Layout'
+// import Home from './Pages/Home'
+// import Layout from './Pages/Layout'
 
 
 function App() {
