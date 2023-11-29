49b67bb9c31e28e577ed0d96d0098e81c1272f63
mattluong <duong.luong2304@gmail.com>
2023-11-29 13:35:40 -0500

    add pages

--- src/boundary/venueLogin.js
+++ src/boundary/venueLogin.js
@@ -1,49 +1,54 @@
 import React, { useState } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
-    const [venueID, setVenueID] = useState('');
-    const [credentials, setCredentials] = useState('');
+    // const [venueID, setVenueID] = useState('');
+    const [credentials, setCredentials] = useState('')
+    const navigate = useNavigate();
   
-    const handleSubmit = () => {
-      // Logic goes here
-      console.log('Authentication:', venueID, credentials);
-    }
+    // const handleLogin = async () => {
+    //   try {
+    //     const response = await fetch('your-authentication-endpoint', {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify({ username, password }),
+    //     });
+  
+    //     if (!response.ok) {
+    //       // Handle authentication error, e.g., show an error message to the user
+    //       console.error('Authentication failed');
+    //       return;
+    //     }
+  
+    //     // Assuming the server responds with a token upon successful authentication
+    //     const data = await response.json();
+    //     const authToken = data.token;
+  
+    //     // Now you can store the authToken in your application state or in a cookie
+    //     // Redirect the user or perform other actions as needed
+    //   } catch (error) {
+    //     console.error('Error during authentication:', error);
+    //   }
+    // };
+    //   // return null console.log('Authentication:', credentials)
+    //   // navigate("/venueLogin")
+    
 
-    // const handleCreateVenue = () => {
-    //   // Logic goes here
-    //   console.log('Redirect user to...')
-    // }
+    const handleCreateVenue = () => {
+      navigate("/createVenue")
+    }
   
     return (
-     <form onSubmit={handleSubmit}>
-
-      {/* <div>
-        <h2>Venue Manager Login</h2>
-        <label>Venue ID: </label>
-        <input type="venueID" value={venueID} onChange={(e) => setVenueID(e.target.value)} />
-      </div> */}
-      
       <div>
-        <label id='password'>Password: </label>
+        <h2 className = "venueLoginHeader">Venue Manager Login</h2>
+        <label className='venueText'>Password: </label>
         <input type="password" value={credentials} onChange={(e) => setCredentials(e.target.value)} />
-        {/* <button onClick={handleLogin}>Login</button>
-        <button onClick={handleCreateVenue}>Create Venue</button> */}
+        
+        {/* <button className = "venueLoginButton" type="submit" onClick={handleLogin}>Login</button> */}
+        <button className = "venueCreateButton" onClick={handleCreateVenue}>Do not have a venue? Create venue</button>
       </div>
-      <button type="submit">Submit</button>
-
-      </form> 
-
-    // <form onSubmit={handleSubmit}>
-    //   <div>
-    //     <label htmlFor="usernameInput">Password: </label>
-    //     <input 
-    //       id="venueLogin" 
-    //       type="password"
-    //     />
-    //   </div>
-
-    //   <button type="submit">Submit</button>
-    // </form>
+    
     )
   }
   
