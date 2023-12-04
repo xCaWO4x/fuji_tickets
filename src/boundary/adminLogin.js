e2a7595acd2913d1ef72329bca72649064feeaea
mattluong <duong.luong2304@gmail.com>
2023-12-04 13:49:26 -0500

    create fetch api for venueLogin(partially)  and adminLogin

--- src/boundary/adminLogin.js
+++ src/boundary/adminLogin.js
@@ -1,25 +1,49 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 const AdminLogin = () => {
-    const [adminId, setAdminID] = useState('');
+    // const [adminId, setAdminID] = useState('');
+    const type = 'Admin'
     const [adminPassword, setAdminPassword] = useState('');
+    const navigate = useNavigate()
+
+    var data = {type:type, credentials:adminPassword}
   
-    const handleAdminLogin = () => {
-    // logic goes here
-      return null
-    }
-  
+    const handleAdminLogin = async () => {
+        try {
+            let payload = {
+              method: 'POST',
+              mode: 'cors', 
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(data),
+            }
+          console.log(payload)
+          const response = await fetch('https://8uwxmxcgd2.execute-api.us-east-2.amazonaws.com/Nov30-2023-Class/fujiwara/login', payload);
+          const answer = await response.json();
+          const status = answer["statusCode"]
+
+          if(status===400){
+            console.error('Authentication failed')
+          }
+          else {
+            navigate('/adminPage')
+          } 
+        }
+        catch (error) {
+            console.error('Error during authentication:', error);
+        }
+}
     return (
       <div>
         <h2 className = "adminLoginHeader">Admin Login</h2>
-        
-         {/* should be admin name? */}
-        <label className='adminText'>Admin Username:  </label>
-        <input type="text" value={adminId} onChange={(e) => setAdminID(e.target.value)} />
+        {/* <label className='adminText'>Admin Username:  </label> */}
+        {/* <input type="text" value={adminId} onChange={(e) => setAdminID(e.target.value)} /> */}
     
         <label className='adminText'>Password: </label>
         <input type="password" value={adminPassword} onChange={(e) => setAdminPassword(e.target.value)} />
-
         <button className = "adminLoginButton" onClick={handleAdminLogin}>Login</button>
       </div>
     
--- src/boundary/venueLogin.js
+++ src/boundary/venueLogin.js
@@ -3,37 +3,42 @@ import { useNavigate } from 'react-router-dom';
 import './venueLogin.css'
 const Login = () => {
     // const [venueID, setVenueID] = useState('');
-    const [credentials, setCredentials] = useState('')
-    const navigate = useNavigate();
     // const url = "https://8uwxmxcgd2.execute-api.us-east-2.amazonaws.com/Nov30-2023-Class"
-   
+    const type = 'VenueManager'
+    const [credentials, setCredentials] = useState('')
+    var data = {type:type, credentials:credentials}
+    const navigate = useNavigate()
+    
     const handleLogin = async () => {
         try {
-          const response = await fetch('https://8uwxmxcgd2.execute-api.us-east-2.amazonaws.com/Nov30-2023-Class/login', {
+          let payload = {
             method: 'POST',
             mode: 'cors', 
             headers: {
               'Content-Type': 'application/json',
             },
-            body: JSON.stringify({credentials}),
-          });
-    
-          if (!response.ok) {
-            // Handle authentication error
-            console.error('Authentication failed');
-            return;
+            body: JSON.stringify(data),
           }
+          console.log(payload)
+          const response = await fetch('https://8uwxmxcgd2.execute-api.us-east-2.amazonaws.com/Nov30-2023-Class/fujiwara/login', payload);
+          const answer = await response.json();
+          const status = answer["statusCode"]
+          // const responseBody = answer["body"]
     
-          const data = await response.json();
-          const authToken = data.token;
+          if (status === 400) {
+            console.error('Authentication failed')
+            // console.log(answer)
+          }
+          else {
+            navigate('/createVenue')
+          }
         } 
         catch (error) {
           console.error('Error during authentication:', error);
         }
       };
 
-
-  // This is handler event for create venue
+      // This is handler event for create venue
     const handleCreateVenue = () => {
       navigate("/createVenue")
     }
