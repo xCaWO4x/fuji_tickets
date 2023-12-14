81e21891fd8d7f64665d4228222c838dfefd9b28
mattluong <duong.luong2304@gmail.com>
2023-12-13 19:11:19 -0500

    Fetch API for listActiveShows (customer) and displays it to canvas

--- src/boundary/customerPage.js
+++ src/boundary/customerPage.js
@@ -46,6 +46,35 @@ const CustomerPage = () => {
     }
   }
 
+  const handleListActiveShows = async () => {
+    try {
+      let payload = {
+        method: 'GET',
+        mode: 'cors', 
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }
+      //console.log(payload)
+      const response = await fetch('https://8uwxmxcgd2.execute-api.us-east-2.amazonaws.com/Nov30-2023-Class/fujiwara/listActiveShows', payload);
+      const answer = await response.json();
+      const status = answer["statusCode"]
+      const responseBody = answer["data"]
+
+      if (status === 400) {
+        // console.log(answer);
+        console.error(responseBody)
+      } else {
+        // setShows(responseBody)
+        console.log(answer)
+        setActiveShows(responseBody)
+      }
+    } 
+    catch (error) {
+      console.error('Error during authentication:', error); 
+    }
+  }
+
 
   const handleCustomerPurchase = () => {
     navigate("/customerPurchase")
@@ -59,12 +88,13 @@ const CustomerPage = () => {
         <input type="text" value={searchShow} placeholder="Search Shows..." onChange={(e) => setSearchShow(e.target.value)}/>
         {/* need to fix button for searchShow */}
         <button onClick={handleSearchShow} className="purchase-button" >Search</button> 
+        <button onClick={handleListActiveShows}>List All Active Shows</button> 
       </div>
       <div className="shows-table">
         {activeShows.map((show) => (
           <div key={show.showID} className="show-row"> 
             <div className="show-cell show-name">{show.name}</div>
-            {/* what is seatAvailable in our data schema?  */}
+            {/* what is seatAvailable in our data schema? show.numSeats */}
             <div className="show-cell seats-available">Seats: {show.numSeats}</div>
             <div className="show-cell purchase-button-cell">
               <button onClick={handleCustomerPurchase} className="purchase-button" >Purchase Tickets</button>
